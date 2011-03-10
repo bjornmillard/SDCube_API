3f52a628ac31986a55095bf95a95d8ccfaf65116
Bjorn Millard <bjornmillard@gmail.com>
2011-03-10 21:57:45 +0000

    added license/copyright info to code

--- src/sdcubeio/ExpDesign_IO.java
+++ src/sdcubeio/ExpDesign_IO.java
@@ -1,3 +1,25 @@
+/**
+   SDCube Programming Library
+   Software for the creation and manipulation of semantically-typed
+   data hypercubes
+
+   Copyright (C) 2011 Bjorn Millard <bjornmillard@gmail.com>
+
+   This program is free software: you can redistribute it and/or modify
+   it under the terms of the GNU Lesser General Public License as
+   published by the Free Software Foundation, either version 3 of the
+   License, or (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU Lesser General Public License for more details.
+
+   You should have received a copy of the GNU Lesser General Public
+   License along with this program.  If not, see
+   <http://www.gnu.org/licenses/>.
+ */
+
 package sdcubeio;
 
 import java.io.BufferedWriter;
@@ -118,7 +140,7 @@ String xmlPath) {
 			// Using factory get an instance of document builder
 			DocumentBuilder db = dbf.newDocumentBuilder();
 			// parse using builder to get DOM representation of the XML file
-			dom = db.parse(new File(xmlPath));
+			dom = db.parse(xmlPath);
 
 		} catch (Exception e) {
 			System.out.println("");
